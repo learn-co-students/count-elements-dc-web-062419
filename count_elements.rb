2c0c0080e5097d16ebdbb08c77e9dfb868daf065
Luke O'Malley <Lukeomalley95@gmail.com>
2019-06-24 18:53:47 -0400

    completed lab

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
 def count_elements(array)
-  # code goes here
+  count = {}
+  array.each do |string|
+    if count.key?(string)
+      count[string] += 1
+    else
+      count[string] = 1
+    end
+  end
+  return count
 end
- 
